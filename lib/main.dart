326e9ad9f00174b3097814c79d41a7100b91a156
palrd <1378802125@qq.com>
2026-02-22 11:35:35 +0800

    进行新页面传值教学

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final String nickName = 'Flutter女神';
-  final int messageCount = 99;
-  final double money = 88.88;
-  final bool isVip = true;
-  final Color themeColors = Colors.green;
   final List<String> friendNames = ['张三', '李四', '王五'];
   final List<String> avatarUrls = [
     'https://picsum.photos/300/400?random=1',
@@ -24,11 +19,22 @@ class _MyAppState extends State<MyApp> {
     'https://picsum.photos/300/400?random=3',
   ];
   final List<double> redPacketHistory = [5.20, 13.14, 66.66];
-  final int age = 8;
-  final String bio = "大家好，我是flutter";
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView.builder(
+          itemCount: friendNames.length,
+          itemBuilder: (_, index) {
+            return Container(
+              margin: EdgeInsets.only(bottom: 20),
+              color: Colors.redAccent,
+              child: Text(friendNames[index]),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
