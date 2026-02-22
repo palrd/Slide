4d3ee926f9492429772412c1c025895927851ad4
palrd <1378802125@qq.com>
2026-02-22 11:00:35 +0800

    2.22操作数组前

--- lib/main.dart
+++ lib/main.dart
@@ -1,178 +1,34 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:teaching_use_1/new_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Color? testColors = Colors.amberAccent; 
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class _MyAppState extends State<MyApp> {
+  final String nickName = 'Flutter女神';
+  final int messageCount = 99;
+  final double money = 88.88;
+  final bool isVip = true;
+  final Color themeColors = Colors.green;
+  final List<String> friendNames = ['张三', '李四', '王五'];
+  final List<String> avatarUrls = [
+    'https://picsum.photos/300/400?random=1',
+    'https://picsum.photos/300/400?random=2',
+    'https://picsum.photos/300/400?random=3',
+  ];
+  final List<double> redPacketHistory = [5.20, 13.14, 66.66];
+  final int age = 8;
+  final String bio = "大家好，我是flutter";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('你好我是appBar'),
-        backgroundColor: const Color.fromARGB(49, 64, 217, 255),
-      ),
-      body: ListView(children: [tapMessage()]),
-    );
-  }
-
-  Widget tapMessage() {
-    return Material(
-      color: Colors.amberAccent,
-      child: InkWell(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) {
-                return NewPage();
-              },
-            ),
-          );
-        },
-        child: Container(child: Message('你好wwww')),
-      ),
-    );
-  }
-
-  Widget Message(String title) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 15),
-      child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.all(15),
-            child: ClipRRect(
-              borderRadius: BorderRadiusGeometry.circular(30),
-              child: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.green, Colors.white],
-                  ),
-                ),
-                child: SizedBox(width: 60, height: 60),
-              ),
-            ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      'PIGSOMEidea',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(width: 15),
-                    Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.message, size: 15, color: Colors.blueGrey),
-                        SizedBox(width: 5),
-                        Text(
-                          title,
-                          style: TextStyle(
-                            color: Colors.blueGrey,
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                Text(
-                  '读懂音乐读懂音乐读懂音乐读懂音乐读懂音乐读懂音乐读懂音乐读懂音乐',
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(
-                    color: const Color.fromARGB(132, 96, 125, 139),
-                    fontSize: 13,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Container(
-                  width: 200,
-                  alignment: AlignmentGeometry.center,
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: const Color.fromARGB(255, 77, 172, 249),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(
-                        Icons.add,
-                        size: 22,
-                        color: const Color.fromARGB(255, 255, 255, 255),
-                      ),
-                      Text(
-                        '关注',
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 15,
-                        ),
-                      ),
-                      SizedBox(width: 5),
-                      Text(
-                        '2099',
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 17,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
