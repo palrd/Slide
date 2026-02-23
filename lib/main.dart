4dc2c7c953ca2c984a2631902da253d793767ff0
palrd <1378802125@qq.com>
2026-02-23 09:28:18 +0800

    2.22

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teaching_use_1/new_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,11 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  ///int double bool String List
+  ///bool false true
+  ///操作一个列表
+  ///列表的名字[下标] --- 取列表里的值
+  ///列表的名字.length   ---计算列表的长度
   final List<String> friendNames = ['张三', '李四', '王五'];
   final List<String> avatarUrls = [
     'https://picsum.photos/300/400?random=1',
@@ -20,17 +26,46 @@ class _MyAppState extends State<MyApp> {
   ];
   final List<double> redPacketHistory = [5.20, 13.14, 66.66];
 
+  final Map<String, String> names = {'name': '张三', 'age': '18', 'sex': '男'};
+  final List<Map<String, String>> namess = [
+    {'name': '张三', 'age': '18', 'sex': '男'},
+    {'name': '张三', 'age': '18', 'sex': '男'},
+    {'name': '张三', 'age': '18', 'sex': '男'}
+  ];
+
+
+
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       home: Scaffold(
         body: ListView.builder(
           itemCount: friendNames.length,
-          itemBuilder: (_, index) {
-            return Container(
-              margin: EdgeInsets.only(bottom: 20),
-              color: Colors.redAccent,
-              child: Text(friendNames[index]),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return NewPage(friendNames[index]);
+                    },
+                  ),
+                );
+              },
+              child: Container(
+                margin: EdgeInsets.only(bottom: 20),
+                color: Colors.redAccent,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(friendNames[index]),
+                    Text(avatarUrls[index]),
+                    Text('${redPacketHistory[index]}'),
+                  ],
+                ),
+              ),
             );
           },
         ),
--- lib/new_page.dart
+++ lib/new_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class NewPage extends StatelessWidget {
-  const NewPage({super.key});
+  final String title;
+  const NewPage(this.title,{super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(
+        child: Text(title),
+      ),
+    );
   }
 }
