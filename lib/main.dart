dd3cfbc375b40581703439ff2786597f5d0c4a0e
palrd <1378802125@qq.com>
2026-02-24 09:49:24 +0800

    2.23

--- lib/main.dart
+++ lib/main.dart
@@ -13,35 +13,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  ///int double bool String List
-  ///bool false true
-  ///操作一个列表
-  ///列表的名字[下标] --- 取列表里的值
-  ///列表的名字.length   ---计算列表的长度
-  final List<String> friendNames = ['张三', '李四', '王五'];
-  final List<String> avatarUrls = [
-    'https://picsum.photos/300/400?random=1',
-    'https://picsum.photos/300/400?random=2',
-    'https://picsum.photos/300/400?random=3',
+
+  
+
+
+
+
+
+  final List<String> songNames = ["七里香", "传奇", "十年", "大海", "后来"];
+  final List<String> singers = ["周杰伦", "王菲", "陈奕迅", "张雨生", "刘若英"];
+  final List<String> comments = [
+    "夏天的味道",
+    "永远的经典",
+    "KTV必点",
+    "听哭的人",
+    "后来学会了如何去爱",
   ];
-  final List<double> redPacketHistory = [5.20, 13.14, 66.66];
-
-  final Map<String, String> names = {'name': '张三', 'age': '18', 'sex': '男'};
-  final List<Map<String, String>> namess = [
-    {'name': '张三', 'age': '18', 'sex': '男'},
-    {'name': '张三', 'age': '18', 'sex': '男'},
-    {'name': '张三', 'age': '18', 'sex': '男'}
-  ];
-
-
+  final List<String> likes = ["3.20", "1.5", "1.2", "1.1", "1.0"];
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.queue_music_sharp, size: 22, color: Colors.black),
+              SizedBox(width: 10),
+              Text(
+                "简易音乐列表",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+        ),
         body: ListView.builder(
-          itemCount: friendNames.length,
+          itemCount: songNames.length,
           itemBuilder: (context, index) {
             return GestureDetector(
               onTap: () {
@@ -49,20 +62,98 @@ class _MyAppState extends State<MyApp> {
                   context,
                   MaterialPageRoute(
                     builder: (context) {
-                      return NewPage(friendNames[index]);
+                      return NewPage(
+                        songNames[index],
+                        singers[index],
+                        comments[index],
+                      );
                     },
                   ),
                 );
               },
               child: Container(
-                margin: EdgeInsets.only(bottom: 20),
-                color: Colors.redAccent,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                margin: EdgeInsets.symmetric(vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: const Color.fromARGB(42, 158, 158, 158),
+                      spreadRadius: 1,
+                      blurRadius: 10,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(friendNames[index]),
-                    Text(avatarUrls[index]),
-                    Text('${redPacketHistory[index]}'),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          width: 80,
+                          height: 80,
+                          margin: EdgeInsets.only(right: 10),
+                          color: Colors.amber,
+                          child: Image.network(
+                            'https://picsum.photos/300/400?random=$index',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              songNames[index],
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              singers[index],
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[600],
+                              ),
+                            ),
+                            SizedBox(height: 5),
+                            Text(
+                              comments[index],
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.favorite_border_sharp,
+                          size: 25,
+                          color: Colors.black,
+                        ),
+
+                        Text(
+                          '${likes[index]}w',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
               ),
--- lib/new_page.dart
+++ lib/new_page.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class NewPage extends StatelessWidget {
-  final String title;
-  const NewPage(this.title,{super.key});
+  final String songName;
+  final String singer;
+  final String comment;
+  const NewPage(this.songName, this.singer, this.comment, {super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
       body: Center(
-        child: Text(title),
+        child: Column(children: [Text(songName), Text(singer), Text(comment)]),
       ),
     );
   }
