fecc1d3b71d089ee4376a236979644987614a18e
palrd <1378802125@qq.com>
2026-02-27 02:19:40 +0800

    浏览页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:teaching_use_1/new_page.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class Theme {
+  static const TextStyle titleBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w900,
+    color: Colors.black,
+  );
+  static const TextStyle titleStyle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+  static const TextStyle subtitle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: Colors.grey,
+  );
 }
 
 class MyApp extends StatefulWidget {
@@ -12,155 +29,187 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-///判断式
-///if(条件){条件为真时要做的事情}else{条件为假时做的事情}
-///三目运算符
-///条件 ? 条件为真时返回的值 : 条件为假时返回的值
-///交互的核心->状态管理->state
-///黑色：没有被点击 -》 false
-///红色：被点击    ——》 true
-///color:isClicked ? Colors.red : Colors.black;
-///final bool isClicked = false;///在一开始是没有被点击过
-///当用户点击了这个爱心，我们就要把isClicked改为true
-///命名规范：在state中的变量，我们一般都加上_，表示这个变量是私有的
-
 class _MyAppState extends State<MyApp> {
-  ///定义一个变量，表示这个组件是否被点击过
-  bool _isClicked = false;
-
-  ///final 表示的是这个变量，在一开始就定义好了，之后不可以被更改
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: GestureDetector(
-            onTap: () {
-              setState(() {
-                _isClicked = !_isClicked;
-              });
-            },
-            child: Text(_isClicked ? "测试真" : "测试假"),
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text(
+            "校园墙@xyq",
+            style: TextStyle(fontWeight: FontWeight.w900, color: Colors.black),
           ),
         ),
-        body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 15),
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              return SigalMessage(index);
-            },
-          ),
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Colors.grey, width: 0.5),
+                  ),
+                ),
+                margin: EdgeInsets.only(bottom: 10),
+                child: ShowTip(),
+              ),
+            ),
+            SliverToBoxAdapter(child: MessageCard()),
+          ],
         ),
       ),
     );
   }
+}
 
-  Widget SigalMessage(int index) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadiusGeometry.circular(40),
-          child: Container(
-            width: 80,
-            height: 80,
-            child: Image.network(
-              'https://picsum.photos/300/400?random=$index',
-              fit: BoxFit.cover,
-            ),
-          ),
+class HeadPortrait extends StatefulWidget {
+  const HeadPortrait({super.key});
+
+  @override
+  State<HeadPortrait> createState() => _HeadPortraitState();
+}
+
+class _HeadPortraitState extends State<HeadPortrait> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadiusGeometry.circular(20),
+      child: SizedBox(
+        width: 40,
+        height: 40,
+        child: Image.network(
+          'https://picsum.photos/300/400?random=1',
+          fit: BoxFit.cover,
         ),
-        SizedBox(width: 15),
-        Expanded(
+      ),
+    );
+  }
+}
+
+class ShowTip extends StatefulWidget {
+  const ShowTip({super.key});
+
+  @override
+  State<ShowTip> createState() => _ShowTipState();
+}
+
+class _ShowTipState extends State<ShowTip> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        HeadPortrait(),
+        SizedBox(width: 10),
+        SizedBox(
+          height: 40,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                "Ryan",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 5),
-              Text(
-                "friend",
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: const Color.fromARGB(255, 168, 168, 168),
-                ),
-              ),
-              SizedBox(height: 15),
-              Text(
-                "Hello,It's my first app！",
-                style: TextStyle(fontSize: 16, color: Colors.black),
-              ),
-              SizedBox(height: 10),
-              SizedBox(
-                height: 230,
-                child: GridView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: 6,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    mainAxisSpacing: 15,
-                    crossAxisSpacing: 15,
-                    childAspectRatio: 1.0,
-                  ),
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => NewPage(),
-                          ),
-                        );
-                      },
-                      child: Hero(
-                        tag: index,
-                        child: Image.network(
-                          'https://picsum.photos/300/400?random=$index',
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '评论',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue[900],
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _isClicked = !_isClicked;
-                      });
-                    },
-                    child: Icon(
-                      Icons.favorite,
-                      color: _isClicked ? Colors.red : Colors.black,
-                      size: 20,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 5),
-              Divider(height: 2, color: Colors.blueGrey),
-              SizedBox(height: 5),
+              Text('paladepa_palrd', style: Theme.titleStyle),
+              Text('分享新鲜事', style: Theme.subtitle),
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class MessageCard extends StatefulWidget {
+  const MessageCard({super.key});
+
+  @override
+  State<MessageCard> createState() => _MessageCardState();
+}
+
+class _MessageCardState extends State<MessageCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 10),
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(color: Colors.grey, width: 0.5)),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                HeadPortrait(),
+                SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('xinn.1777', style: Theme.titleStyle),
+                          SizedBox(width: 20),
+                          Text('7小时', style: Theme.subtitle),
+                        ],
+                      ),
+                      SizedBox(height:  5),
+                      Text(
+                        '这将是我测试的文本内容！',
+                        style: Theme.titleBold,
+                        maxLines: 50,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      SizedBox(height: 5),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 200,
+            width: double.infinity,
+            child: ListView.builder(
+              padding: EdgeInsets.only(left: 60),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 150,
+                  height: 200,
+                  margin: EdgeInsets.only(right: 8),
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    border: Border.all(
+                      color: const Color.fromARGB(129, 158, 158, 158),
+                      width: 0.5,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color.fromARGB(144, 158, 158, 158),
+                        blurRadius: 1,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
+                  ),
+                  child: Image.network(
+                    'https://picsum.photos/300/400?random=$index',
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
