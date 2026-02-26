69781c9f719b7a77ecdc25ed838c3b0d32911db2
palrd <1378802125@qq.com>
2026-02-26 10:51:59 +0800

    2.25

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:teaching_use_1/new_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,17 +12,42 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+///判断式
+///if(条件){条件为真时要做的事情}else{条件为假时做的事情}
+///三目运算符
+///条件 ? 条件为真时返回的值 : 条件为假时返回的值
+///交互的核心->状态管理->state
+///黑色：没有被点击 -》 false
+///红色：被点击    ——》 true
+///color:isClicked ? Colors.red : Colors.black;
+///final bool isClicked = false;///在一开始是没有被点击过
+///当用户点击了这个爱心，我们就要把isClicked改为true
+///命名规范：在state中的变量，我们一般都加上_，表示这个变量是私有的
+
 class _MyAppState extends State<MyApp> {
+  ///定义一个变量，表示这个组件是否被点击过
+  bool _isClicked = false;
+
+  ///final 表示的是这个变量，在一开始就定义好了，之后不可以被更改
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("我的测试软件")),
+        appBar: AppBar(
+          title: GestureDetector(
+            onTap: () {
+              setState(() {
+                _isClicked = !_isClicked;
+              });
+            },
+            child: Text(_isClicked ? "测试真" : "测试假"),
+          ),
+        ),
         body: Container(
           padding: EdgeInsets.symmetric(horizontal: 15),
           child: ListView.builder(
-            itemBuilder: (_, index) {
-              return SigalMessage();
+            itemBuilder: (context, index) {
+              return SigalMessage(index);
             },
           ),
         ),
@@ -29,90 +55,112 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-  Widget SigalMessage() {
-    return Container(
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadiusGeometry.circular(35),
-            child: Container(
-              width: 70,
-              height: 70,
-              child: Image.network(
-                'https://www.dmoe.cc/random.php',
-                fit: BoxFit.cover,
-              ),
+  Widget SigalMessage(int index) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadiusGeometry.circular(40),
+          child: Container(
+            width: 80,
+            height: 80,
+            child: Image.network(
+              'https://picsum.photos/300/400?random=$index',
+              fit: BoxFit.cover,
             ),
           ),
-          SizedBox(width: 10),
-          Expanded(
-            child: Container(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Ryan',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
+        ),
+        SizedBox(width: 15),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Ryan",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                "friend",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 168, 168, 168),
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                "Hello,It's my first app！",
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                height: 230,
+                child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 6,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 15,
+                    childAspectRatio: 1.0,
                   ),
-                  Text(
-                    'Friend',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: const Color.fromARGB(255, 137, 137, 137),
-                    ),
-                  ),
-                  Text(
-                    '你好这是我的第一个软件',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: const Color.fromARGB(255, 137, 137, 137),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 300,
-                    child: GridView.builder(
-                      itemCount: 6,
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        mainAxisSpacing: 15,
-                        crossAxisSpacing: 15,
-                        childAspectRatio: 1.0,
-                      ),
-                      itemBuilder: (context, index) {
-                        return Image.network(
-                          'https://www.dmoe.cc/random.php',
-                          fit: BoxFit.cover,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => NewPage(),
+                          ),
                         );
                       },
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '回复',
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.normal,
-                          color: Colors.blue[900],
+                      child: Hero(
+                        tag: index,
+                        child: Image.network(
+                          'https://picsum.photos/300/400?random=$index',
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      Icon(Icons.favorite, size: 20),
-                    ],
+                    );
+                  },
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '评论',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue[900],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _isClicked = !_isClicked;
+                      });
+                    },
+                    child: Icon(
+                      Icons.favorite,
+                      color: _isClicked ? Colors.red : Colors.black,
+                      size: 20,
+                    ),
                   ),
                 ],
               ),
-            ),
+              SizedBox(height: 5),
+              Divider(height: 2, color: Colors.blueGrey),
+              SizedBox(height: 5),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
--- lib/new_page.dart
+++ lib/new_page.dart
@@ -1,18 +1,48 @@
 import 'package:flutter/material.dart';
 
-class NewPage extends StatelessWidget {
-  final Map<String, String> songInfo;
-  const NewPage(this.songInfo, {super.key});
+class NewPage extends StatefulWidget {
+  const NewPage({super.key});
+
+  @override
+  State<NewPage> createState() => _NewPageState();
+}
+
+class _NewPageState extends State<NewPage> {
+  final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(songInfo["songName"]!)),
-      body: ListTile(
-        leading: Image.network(songInfo["image"]!, fit: BoxFit.cover),
-        title: Text(songInfo["singer"]!),
-        subtitle: Text(songInfo["songName"]!),
-        trailing: Text(songInfo["comment"]!),
+      floatingActionButton: ElevatedButton(
+        onPressed: () {
+          print(_controller.value);
+          print(_controller.text);
+        },
+        child: Icon(Icons.search),
+      ),
+      appBar: AppBar(title: const Text("Snap Gallery")),
+      body: Align(
+        alignment: AlignmentGeometry.topRight,
+        child: SizedBox(
+          width: 200,
+          height: double.infinity,
+          child: TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: '聊聊新鲜事',
+              hintStyle: TextStyle(
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            cursorOpacityAnimates: true,
+            cursorRadius: Radius.circular(2),
+            cursorWidth: 2,
+            cursorColor: Colors.black,
+            maxLines: null,
+          ),
+        ),
       ),
     );
   }
